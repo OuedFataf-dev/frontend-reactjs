493bc1a649d533cfb9dc9452e71529f03c7e8602
fataf <ouedraogofataf75@gmail.com>
2025-01-15 16:10:43 +0100

    modification du contenu des fichier login et inscription

--- src/components/inscription.jsx
+++ src/components/inscription.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { Card, Input, Checkbox, Button, Typography } from "@material-tailwind/react";
 import { useNavigate } from "react-router-dom";
 
 function SimpleRegistrationForm() {
@@ -41,88 +40,145 @@ function SimpleRegistrationForm() {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <Card color="transparent" shadow={false} className="w-96">
-        <Typography variant="h4" color="blue-gray">
-          S'inscrire
-        </Typography>
-        <Typography color="gray" className="mt-1 font-normal">
-          Enchanté de vous rencontrer ! Entrez vos détails pour vous inscrire.
-        </Typography>
-        <form className="flex flex-col gap-4 md:mt-12" onSubmit={handleSubmit}>
-          <div className="flex flex-col">
-            <Typography variant="h6" color="blue-gray" className="mb-1">
-              Nom
-            </Typography>
-            <Input
-              size="lg"
-              placeholder="Votre Nom"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className="!border-t-blue-gray-200 focus:!border-t-gray-900 mb-4"
-              labelProps={{
-                className: "before:content-none after:content-none",
-              }}
-            />
-            <Typography variant="h6" color="blue-gray" className="mb-1 mt-4">
-              Votre Email
-            </Typography>
-            <Input type="email"
-              size="lg"
-              placeholder="name@mail.com"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="!border-t-blue-gray-200 focus:!border-t-gray-900 mb-4"
-              labelProps={{
-                className: "before:content-none after:content-none",
-              }}
-            />
-            <Typography variant="h6" color="blue-gray" className="mb-1 mt-4">
-              Mot de passe
-            </Typography>
-            <Input
-              type="password"
-              size="lg"
-              placeholder="********"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="!border-t-blue-gray-200 focus:!border-t-gray-900 mb-4"
-              labelProps={{
-                className: "before:content-none after:content-none",
-              }}
-            />
-          </div>
-          <Checkbox
-            label={
-              <Typography
-                variant="small"
-                color="gray"
-                className="flex items-center font-normal"
-              >
-                J'accepte les
+    <div className="font-[sans-serif] bg-white md:h-screen">
+      <div className="grid md:grid-cols-2 items-center gap-8 h-full">
+        <div className="max-md:order-1 p-4">
+          <img
+            src="https://readymadeui.com/signin-image.webp"
+            className="lg:max-w-[85%] w-full h-full aspect-square object-contain block mx-auto"
+            alt="login-image"
+          />
+        </div>
+
+        <div className="flex items-center md:p-8 p-6 bg-[#0C172C] h-full lg:w-11/12 lg:ml-auto">
+          <form className="max-w-lg w-full mx-auto" onSubmit={handleSubmit}>
+            <div className="mb-12">
+              <h3 className="text-2xl font-bold text-yellow-400">Create an account</h3>
+            </div>
+
+            <div>
+              <label className="text-white text-xs block mb-2" htmlFor="name">Full Name</label>
+              <div className="relative flex items-center">
+                <input
+                  name="name"
+                  type="text"
+                  required
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  className="w-full bg-transparent text-sm text-white border-b border-gray-300 focus:border-yellow-400 pl-2 pr-8 py-3 outline-none"
+                  placeholder="Enter name"
+                />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="#bbb"
+                  stroke="#bbb"
+                  className="w-[18px] h-[18px] absolute right-2"
+                  viewBox="0 0 24 24"
+                >
+                  <circle cx="10" cy="7" r="6" data-original="#000000"></circle>
+                  <path
+                    d="M14 15H6a5 5 0 0 0-5 5 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 5 5 0 0 0-5-5zm8-4h-2.59l.3-.29a1 1 0 0 0-1.42-1.42l-2 2a1 1 0 0 0 0 1.42l2 2a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42l-.3-.29H22a1 1 0 0 0 0-2z"
+                    data-original="#000000"
+                  ></path>
+                </svg>
+              </div>
+            </div>
+
+            <div className="mt-8">
+              <label className="text-white text-xs block mb-2" htmlFor="email">Email</label>
+              <div className="relative flex items-center">
+                <input
+                  name="email"
+                  type="text"
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="w-full bg-transparent text-sm text-white border-b border-gray-300 focus:border-yellow-400 pl-2 pr-8 py-3 outline-none"
+                  placeholder="Enter email"
+                />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="#bbb"
+                  stroke="#bbb"
+                  className="w-[18px] h-[18px] absolute right-2"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="none"
+                    strokeMiterlimit="10"
+                    strokeWidth="40"
+                    d="M452 444H60c-22.091 0-40-17.909-40-40v-39.446l212.127-157.782c14.17-10.54 33.576-10.54 47.746 0L492 364.554V404c0 22.091-17.909 40-40 40Z"
+                    data-original="#000000"
+                  ></path>
+                </svg>
+              </div>
+            </div>
+
+            <div className="mt-8">
+              <label className="text-white text-xs block mb-2" htmlFor="password">Password</label>
+              <div className="relative flex items-center">
+                <input
+                  name="password"
+                  type="password"
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="w-full bg-transparent text-sm text-white border-b border-gray-300 focus:border-yellow-400 pl-2 pr-8 py-3 outline-none"
+                  placeholder="Enter password"
+                />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="#bbb"
+                  stroke="#bbb"
+                  className="w-[18px] h-[18px] absolute right-2 cursor-pointer"
+                  viewBox="0 0 128 128"
+                >
+                  <path
+                    d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z"
+                    data-original="#000000"
+                  ></path>
+                </svg>
+              </div>
+            </div>
+
+            <div className="flex items-center mt-8">
+              <input
+                id="remember-me"
+                name="remember-me"
+                type="checkbox"
+                className="h-4 w-4 shrink-0 rounded"
+                checked={agreeTerms}
+                onChange={(e) => setAgreeTerms(e.target.checked)}
+              />
+              <label htmlFor="remember-me" className="text-white ml-3 block text-sm">
+                I accept the{" "}
                 <a
                   href="#"
-                  className="font-medium transition-colors hover:text-gray-900"
+                  className="text-yellow-500 font-semibold hover:underline ml-1"
+                  onClick={(e) => e.preventDefault()} // Cette ligne empêche le comportement par défaut du lien
                 >
-                  &nbsp;termes et conditions
+                  Terms and Conditions
                 </a>
-              </Typography>
-            }
-            checked={agreeTerms}
-            onChange={(e) => setAgreeTerms(e.target.checked)}
-            containerProps={{ className: "-ml-2.5" }}
-          />
-          <Button className="mt-6" fullWidth type="submit">
-            S'inscrire
-          </Button>
-          <Typography color="gray" className="mt-4 text-center font-normal">
-            Vous avez déjà un compte ?{" "}
-            <a href="/login" className="font-medium text-gray-900">
-              Se connecter
-            </a>
-          </Typography>
-        </form>
-      </Card>
+              </label>
+            </div>
+
+            <div className="mt-8">
+              <button
+                type="submit"
+                className="w-max shadow-xl py-3 px-6 text-sm text-gray-800 font-semibold rounded bg-transparent bg-yellow-400 hover:bg-yellow-500 focus:outline-none"
+              >
+                Register
+              </button>
+              <p className="text-sm text-white mt-8">
+                Already have an account?{" "}
+                <a href="/login" className="text-yellow-400 font-semibold hover:underline ml-1">
+                  Login here
+                </a>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
